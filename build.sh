1458bacdeb176af37032e65258e3205f1c48df61
sleechengn <sleechengn@live.com>
2024-05-16 00:24:33 +0000

    更新脚本

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
-docker build $(dirname $0) --filename Dockerfile -t sleechengn/comfyui:nvidia-gpu-full
+IMAGE_TAG=$1
+if [$IMAGE_TAG] then;
+		docker build $(dirname $0) --filename Dockerfile -t $IMAGE_TAG
+	else
+		docker build $(dirname $0) --filename Dockerfile -t sleechengn/comfyui:nvidia-gpu-full
+fi
