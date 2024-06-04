f6c905c3ad0b786819f55f2a0fad7fe74d00c9cb
sleechengn <sleechengn@live.com>
2024-06-04 05:03:14 +0000

    添加插件

--- settings.sh
+++ settings.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 apt update
+apt remove python*
+apt autoremove
 apt -y install openssh-server nano unzip wget curl psmisc net-tools git python3.11 python3-pip nginx fonts-noto-cjk-extra ffmpeg ttyd python3.11-dev
 
 # configure ssh-server
