661bb5b9c5908774a3f8a89c7275d9fccd90c3c6
Der.L <sleechengn@hotmail.com>
2024-10-09 14:40:34 +0000

    add plugi

--- settings.sh
+++ settings.sh
@@ -3,7 +3,7 @@
 apt update
 apt remove python*
 apt autoremove
-apt -y install openssh-server nano unzip wget curl psmisc net-tools git python3.11 python3-pip nginx fonts-noto-cjk-extra ffmpeg ttyd python3.11-dev
+apt -y install openssh-server nano unzip wget curl psmisc net-tools git python3.11 python3-pip nginx fonts-noto-cjk-extra ffmpeg ttyd python3.11-dev 
 
 # configure ssh-server
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
---
+++ stage-fix-impact.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+python3.11 /opt/ComfyUI/custom_nodes/ComfyUI-Impact-Pack/install.py
