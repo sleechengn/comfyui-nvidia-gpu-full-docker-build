7902fd4fb6a4a1d14d903f80b26ce1dec013c535
Der.L <sleechengn@hotmail.com>
2024-11-02 14:01:03 +0800

    add

--- stage-plugin-ComfyUI_kkTranslator_nodes.sh
+++ stage-plugin-ComfyUI_kkTranslator_nodes.sh
@@ -2,5 +2,8 @@
 set -e
 cd /opt/ComfyUI/custom_nodes
 git clone https://github.com/AIGCTeam/ComfyUI_kkTranslator_nodes
+sed -i '/hashlib/d' /opt/ComfyUI/custom_nodes/ComfyUI_kkTranslator_nodes/requirements.txt
+sed -i '/random/d' /opt/ComfyUI/custom_nodes/ComfyUI_kkTranslator_nodes/requirements.txt
+sed -i '/json/d' /opt/ComfyUI/custom_nodes/ComfyUI_kkTranslator_nodes/requirements.txt
 python3.11 -m pip install -r /opt/ComfyUI/custom_nodes/ComfyUI_kkTranslator_nodes/requirements.txt
 cd /opt/ComfyUI/custom_nodes/ComfyUI_kkTranslator_nodes
