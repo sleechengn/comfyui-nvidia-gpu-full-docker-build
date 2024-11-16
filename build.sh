9ab0ebfa24883de933a059c1b301815e457333ac
Der.L <sleechengn@hotmail.com>
2024-11-16 16:32:46 +0800

    add

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 IMAGE_TAG=$1
+echo build [$(dirname $0)]  Dockerfile.stage.build
 if [ $IMAGE_TAG ]; then
 		docker build $(dirname $0) --file Dockerfile.stage.build --target prod -t $IMAGE_TAG
 	else
---
+++ build310.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+set -e
+
+rm -rf /opt/tmp/comfyui-build-310
+/usr/bin/cp -ra . /opt/tmp/comfyui-build-310
+
+cd /opt/tmp/comfyui-build-310
+echo current $(pwd)
+rm -rf .git
+sed -i 's,python3\.11,python3.10,g' Dockerfile.stage.build
+sed -i 's,python3\.11,python3.10,g' init.sh
+sed -i 's,python3\.11,python3.10,g' settings.sh
+find .|grep stage|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
+
+./build.sh
