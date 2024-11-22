61b01661cf558e2143fe780fa4c943c6effc12fe
Der.L <sleechengn@hotmail.com>
2024-11-22 11:38:35 +0800

    add

--- settings.sh
+++ settings.sh
@@ -5,7 +5,7 @@ set -e
 apt update
 apt remove python*
 apt autoremove
-apt -y install openssh-server curl git python3.11 python3-pip nginx fonts-noto-cjk-extra ffmpeg ttyd python3.11-dev libglfw3-dev libgles2-mesa-dev freeglut3-dev
+apt -y install wget openssh-server curl git python3.11 python3-pip nginx fonts-noto-cjk-extra ffmpeg ttyd python3.11-dev libglfw3-dev libgles2-mesa-dev freeglut3-dev
 
 # configure ssh-server
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
@@ -23,3 +23,4 @@ echo "/opt/ComfyUI/python-lib" > /usr/local/lib/python3.11/dist-packages/python3
 rm -rf /etc/nginx/sites-enabled/*
 python3.11 -m pip cache purge
 apt autoremove
+
--- stage-fix-SageAttention.sh
+++ stage-fix-SageAttention.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
-
 set -e
-
 cd /opt
+export CUDA_HOME=/usr/local/cuda-12.6
 git clone https://github.com/thu-ml/SageAttention
 cd /opt/SageAttention
-python3.11 -m pip install .
+python3.11 -m pip install -e .
 cd /opt
