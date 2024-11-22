6071375b0215460a285e6cab3ae80cfb7b129d6a
alicia <sleechengn@live.cn>
2024-11-22 16:13:59 +0800

    add

--- stage-plugin-ComfyUI-MimicMotion.sh
+++ stage-plugin-ComfyUI-MimicMotion.sh
@@ -3,4 +3,4 @@ set -e
 cd /opt/ComfyUI/custom_nodes
 git clone https://github.com/AIFSH/ComfyUI-MimicMotion
 python3.11 -m pip install -r /opt/ComfyUI/custom_nodes/ComfyUI-MimicMotion/requirements.txt
-#cd /opt/ComfyUI/custom_nodes/ComfyUI-MimicMotion
+cd /opt/ComfyUI/custom_nodes/ComfyUI-MimicMotion
