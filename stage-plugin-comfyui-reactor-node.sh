f4a02cccfe4090980d45ba60e23841b4cd34446b
Der.L <sleechengn@hotmail.com>
2025-01-21 10:21:54 +0800

    add

--- stage-plugin-comfyui-reactor-node.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/bash
-set -e
-cd /opt/ComfyUI/custom_nodes
-git clone https://github.com/Gourieff/comfyui-reactor-node
-python3.11 -m pip install -r /opt/ComfyUI/custom_nodes/comfyui-reactor-node/requirements.txt
-cd /opt/ComfyUI/custom_nodes/comfyui-reactor-node
-python3.11 install.py
