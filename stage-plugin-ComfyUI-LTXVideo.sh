59f3921bb794b30dbc553aad08eac61a9c01db06
Der.L <sleechengn@hotmail.com>
2025-02-03 21:53:16 +0800

    add

--- stage-plugin-ComfyUI-LTXVideo.sh
+++ stage-plugin-ComfyUI-LTXVideo.sh
@@ -2,8 +2,5 @@
 set -e
 cd /opt/ComfyUI/custom_nodes
 git clone https://github.com/Lightricks/ComfyUI-LTXVideo
-#python3.11 -m pip install -r /opt/ComfyUI/custom_nodes/ComfyUI-LTXVideo/requirements.txt
-python3.11 -m pip install ltx-video@git+https://github.com/Lightricks/LTX-Video.git@ltx-video-0.9
-python3.11 -m pip install comfyui-easynodes
-python3.11 -m pip install huggingface_hub==0.25.2
+python3.11 -m pip install -r /opt/ComfyUI/custom_nodes/ComfyUI-LTXVideo/requirements.txt
 cd /opt/ComfyUI/custom_nodes/ComfyUI-LTXVideo
