93fca0d5acafe556a7bfeb324e53a8f57198abd3
Der.L <sleechengn@hotmail.com>
2025-02-08 16:48:20 +0800

    create update and fix

--- build-python3.10.sh
+++ build-python3.10.sh
@@ -14,5 +14,6 @@ sed -i 's,python3\.11,python3.10,g' settings.sh
 find .|grep stage|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
 find ./installer/|grep .sh|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
 
-./build.sh
+./build.sh 192.168.13.73:5000/sleechengn/comfyui:nvidia-gpu-full
 rm -rf /opt/tmp/comfyui-build-310
+docker push 192.168.13.73:5000/sleechengn/comfyui:nvidia-gpu-full
