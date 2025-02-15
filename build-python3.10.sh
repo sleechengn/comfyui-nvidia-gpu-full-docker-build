9401f2c4a3ff262de55b48ba32583030236b403c
Der.L <sleechengn@hotmail.com>
2025-02-15 09:38:46 +0800

    create update and fix

--- build-python3.10.sh
+++ build-python3.10.sh
@@ -10,7 +10,7 @@ pushd $TGT_DIR
 echo current $TGT_DIR
 rm -rf .git
 sed -i 's,python3\.11,python3.10,g' Dockerfile.stage.build
-sed -i 's,python3\.11,python3.10,g' init.sh
+sed -i 's,python3\.11,python3.10,g' docker-entrypoint.sh
 sed -i 's,python3\.11,python3.10,g' settings.sh
 find .|grep stage|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
 find ./installer/|grep .sh|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
