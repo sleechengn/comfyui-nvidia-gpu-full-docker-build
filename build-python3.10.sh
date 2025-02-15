7209f6f63003ad49e01421210bfbf25f214a9ac6
Der.L <sleechengn@hotmail.com>
2025-02-15 09:33:35 +0800

    create update and fix

--- build-python3.10.sh
+++ build-python3.10.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 set -e
 
-rm -rf /opt/tmp/comfyui-build-310
-mkdir -p /opt/tmp
-/usr/bin/cp -ra . /opt/tmp/comfyui-build-310
+TGT_PRX=/opt/tmp
+TGT_DIR=/opt/tmp/comfyui-build-310
+rm -rf $TGT_DIR && mkdir -p $TGT_PRX
+/usr/bin/cp -ra $(dirname $0) $TGT_DIR
 
-cd /opt/tmp/comfyui-build-310
-echo current $(pwd)
+pushd $TGT_DIR
+echo current $TGT_DIR
 rm -rf .git
 sed -i 's,python3\.11,python3.10,g' Dockerfile.stage.build
 sed -i 's,python3\.11,python3.10,g' init.sh
 sed -i 's,python3\.11,python3.10,g' settings.sh
 find .|grep stage|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
 find ./installer/|grep .sh|grep -v grep|xargs -i sed -i 's,python3\.11,python3.10,g' {}
-
 ./build.sh 192.168.13.73:5000/sleechengn/comfyui:nvidia-gpu-full
-rm -rf /opt/tmp/comfyui-build-310
+popd
+
+rm -rf $TGT_DIR
 docker push 192.168.13.73:5000/sleechengn/comfyui:nvidia-gpu-full
